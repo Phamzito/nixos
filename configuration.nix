bb18af6cc8394e0a19a012ac2fa9a9ce68cbb18a
David Gustavo Herrera De La Cruz <phamzito@outlook.com>
2026-01-30 04:22:27 -0600

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -16,7 +16,7 @@
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_xanmod_latest;
   boot.kernelModules = [ "ntsync" ];
-  boot.kernelParams = [ "ntsync.init=1" "amdgpu.ppfeaturemask=0xffffffff" ];
+  boot.kernelParams = [ "amdgpu.ppfeaturemask=0xffffffff" ];
   boot.kernel.sysctl = {
     "kernel.sched_latency_ns" = 6000000;
     "kernel.sched_min_granularity_ns" = 750000;
