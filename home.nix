eda52c450ad7af23d715803317a46b9ff73919e3
David Gustavo Herrera De La Cruz <phamzito@outlook.com>
2026-01-29 23:07:28 -0600

    Modify user settings and package list in home.nix
    Updated user packages and configurations in home.nix.

--- home.nix
+++ home.nix
@@ -1,12 +1,17 @@
 { config, pkgs, ... }:
 
 {
+  ## Usuario
   home.username = "pham";
   home.homeDirectory = "/home/pham";
+
+  ## Recursos X (cursor, DPI)
   xresources.properties = {
     "Xcursor.size" = 24;
     "Xft.dpi" = 96;
   };
+
+  ## Discord (OpenASAR + Vencord)
   programs.discord = {
     enable = true;
     package = pkgs.discord.override {
@@ -14,9 +19,21 @@
       withVencord = true;
     };
   };
+
+  ## Paquetes de usuario
   home.packages = with pkgs; [
+    # Utilidades
     fastfetch
     nnn
+    zip
+    xz
+    unzip
+    p7zip
+    file
+    which
+    tree
+
+    # Gaming
     mangohud
     lutris
     protonplus
@@ -25,19 +42,22 @@
     hydralauncher
     wineWowPackages.staging
     bottles
+    heroic
+
+    # Navegador y seguridad
     brave
     keepassxc
-    zip
-    xz
-    unzip
-    p7zip
-    heroic
+
+    # CLI moderna
     ripgrep
     jq
     yq-go
     eza
     fzf
+    glow
+    nix-output-monitor
 
+    # Red
     mtr
     iperf3
     dnsutils
@@ -47,33 +67,43 @@
     nmap
     ipcalc
 
-    file
-    which
-    tree
-    gnused
-    gnutar
-    gawk
-    zstd
-    gnupg
-
-    nix-output-monitor
-
-    glow
-
+    # Sistema
     btop
     iotop
     iftop
-    strace
-    ltrace
-    lsof
-
     sysstat
     lm_sensors
     ethtool
     pciutils
     usbutils
+
+    # Debug
+    strace
+    ltrace
+    lsof
+
+    # GNU utils
+    gnused
+    gnutar
+    gawk
+    zstd
+    gnupg
   ];
 
+  ## GNOME Shell (extensiones)
+  programs.gnome-shell = {
+    enable = true;
+
+    extensions = with pkgs.gnomeExtensions; [
+      # Reduce input lag en fullscreen
+      disable-unredirect-fullscreen-windows
+
+      # Quita el overview al iniciar sesión
+      no-overview-at-startup
+    ];
+  };
+
+  ## Git
   programs.git = {
     enable = true;
     settings = {
@@ -82,33 +112,38 @@
     };
   };
 
+  ## Prompt (Starship)
   programs.starship = {
     enable = true;
     settings = {
       add_newline = false;
+      line_break.disabled = true;
       aws.disabled = true;
       gcloud.disabled = true;
-      line_break.disabled = true;
     };
   };
 
+  ## Terminal (Alacritty)
   programs.alacritty = {
     enable = true;
-    # custom settings
     settings = {
       env.TERM = "xterm-256color";
+
       font = {
         size = 12;
         draw_bold_text_with_bright_colors = true;
       };
+
       scrolling.multiplier = 5;
       selection.save_to_clipboard = true;
     };
   };
 
+  ## Bash
   programs.bash = {
     enable = true;
     enableCompletion = true;
+
     bashrcExtra = ''
       export PATH="$PATH:$HOME/bin:$HOME/.local/bin:$HOME/go/bin"
     '';
@@ -120,5 +155,6 @@
     };
   };
 
+  ## Versión de Home Manager (NO tocar)
   home.stateVersion = "25.11";
 }
