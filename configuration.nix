9239f619de85112e78f455c8123080c5d94ad3c1
David Gustavo Herrera De La Cruz <phamzito@outlook.com>
2026-01-30 22:34:41 -0600

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -15,7 +15,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.kernelPackages = pkgs.linuxPackages_xanmod_latest;
-  boot.kernelModules = [ "ntsync" ];
+  boot.kernelModules = [ "ntsync" "k10temp" ];
   boot.kernelParams = [
     "usbhid.kbpoll=1"
     "usbhid.mousepoll=1"    
